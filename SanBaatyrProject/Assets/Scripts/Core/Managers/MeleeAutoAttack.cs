a57755da198888459db065b17ad9e85d70a046f1
tensai.timur <tensai.timur@gmail.com>
2019-12-23 21:41:59 +0600

    Add melee combat
    Добавил ближнюю автоматическую атаку, атакует объекты с определенным layer (задается в настройке скрипта)

---
+++ SanBaatyrProject/Assets/Scripts/Core/Managers/MeleeAutoAttack.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core.Enemies;
+using UnityEngine;
+
+namespace Core.Managers
+{
+    public class MeleeAutoAttack : MonoBehaviour
+    {
+        public Animator animator;
+
+        public Transform attackPoint;
+        public LayerMask enemyLayers;
+
+        public float detectRange;
+        public float attackRange;
+
+        public int attackDamage;
+        public float attackRate;
+
+        private float nextAttackTime = 0f;
+        private Collider2D[] hitEnemys;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Time.time >= nextAttackTime)
+            {
+                if (EnemyDetected())
+                {
+                    Attack();
+                    nextAttackTime = Time.time + 1f / attackRate;
+                }
+            }
+        }
+
+        bool EnemyDetected()
+        {
+            hitEnemys = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            if (hitEnemys.Length == 0)
+                return false;
+            else
+                return true;
+        }
+
+        void Attack()
+        {
+            animator.SetTrigger("Attack");
+
+            //Collider2D[] hitEnemys = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemys)
+            {
+                enemy.GetComponent<Enemy>().DealDamage(attackDamage);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (attackPoint == null)
+                return;
+
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+            Gizmos.DrawWireSphere(attackPoint.position, detectRange);
+        }
+    }
+}
