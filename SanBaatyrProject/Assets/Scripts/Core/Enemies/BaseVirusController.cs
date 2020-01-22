fc556d44572c8351df8c24fa9fab19f72d748497
Vladimir <vladimir.nedoroslev@gmail.com>
2020-01-23 00:23:41 +0600

    Added BaseVirusController class;

---
+++ SanBaatyrProject/Assets/Scripts/Core/Enemies/BaseVirusController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Core.Player;
+using Core.Health;
+using Core.ScriptableObjects;
+using Interfaces;
+using Pathfinding;
+using UnityEngine;
+
+namespace Core.Enemies
+{
+    public class BaseVirusController : MonoBehaviour, IPooledObject
+    {
+        public EnemyData enemyData;
+        public BaseHealthBehavior health;
+        public PlayerController player;
+
+        private Animator _animator;
+        private float _attackCooldownTime = 1.0f;
+
+        public void Start()
+        {
+            health = new BaseHealthBehavior(enemyData.maxHealth, enemyData.maxHealth);
+            OnObjectSpawn();
+        }
+
+        public void OnObjectSpawn()
+        {
+//            gameObject.GetComponent<AIPath>().maxSpeed = enemyData.speed;
+            _animator = gameObject.GetComponent<Animator>();
+        }
+
+    }
+}
