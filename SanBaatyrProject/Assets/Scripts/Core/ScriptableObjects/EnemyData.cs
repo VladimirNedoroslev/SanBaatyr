b5e83cf78c464ae58b04498889eec0cdaff857c5
Vladimir <vladimir.nedoroslev@gmail.com>
2020-01-23 00:27:29 +0600

    Added attackSpeed to EnemyData

--- SanBaatyrProject/Assets/Scripts/Core/ScriptableObjects/EnemyData.cs
+++ SanBaatyrProject/Assets/Scripts/Core/ScriptableObjects/EnemyData.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Resources.ScriptableObjects
+namespace Core.ScriptableObjects
 {
     [CreateAssetMenu(fileName = "NewEnemyData", menuName = "New Enemy data")]
     public class EnemyData : ScriptableObject
@@ -8,6 +8,7 @@ namespace Resources.ScriptableObjects
         public int maxHealth;
         public int damage;
         public float speed;
+        public float attackSpeed;
         public int visionDistance;
     }
 }
